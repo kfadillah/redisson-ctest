1185cd71e5366349e711c54173ea11d95e9c6a14
kfadillah <fadillahmichael@hotmail.com>
2022-11-02 12:07:11 -0500

    patch

--- identify_param/setup_redisson.sh
+++ identify_param/setup_redisson.sh
@@ -1,4 +1,4 @@
 [ ! -d "app/cassandra" ] && git clone https://github.com/redisson/redisson.git app/redisson
 cd app/redisson
 git checkout 34bcddc
-#git apply ../../ctest-logging.patch
+git apply ../../ctest-logging.patch
